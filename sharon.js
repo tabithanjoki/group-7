7c3a2db2cdef5f38599c6d60489eca0b4397ffe9
Tabitha Njoki <t8080438@gmail.com>
2026-04-23 11:06:50 +0300

    Refactor formatting in the fizzbuzz implementation for improved readability

--- sharon.js
+++ sharon.js
@@ -2,9 +2,9 @@
 // If a number is divisible by 3, print "fizz" instead
 
 for (let i = 1; i <= 50; i++) {
-    if (i % 3 === 0) {
-        console.log("fizz");
-    } else {
-        console.log(i);
-    }
+  if (i % 3 === 0) {
+    console.log("fizz");
+  } else {
+    console.log(i);
+  }
 }
